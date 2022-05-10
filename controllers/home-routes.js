e44820e7638664395b79cfc3b5022dbfc4dfa6f3
Mayur Patel <pmayur0680@gmail.com>
2022-05-10 09:30:59 -0500

    get routes for category and questions

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,28 @@
 // Routes for home page goes here
 const router = require('express').Router();
-// const { Post, Comment, User } = require('../models/'); // change model names
+const Category  = require('../models/category');
 // // Import the custom middleware
-// const withAuth = require('../utils/auth'); // 
+ const withAuth = require('../utils/auth'); 
 
-// get for home page
+// Author: Mayur
+// Purpose: Get route to fetch list of all categories order by category_name from database
  router.get('/', async (req, res) => {  
-    res.render('index','');
+  try {
+    const categoryData = await Category.findAll({     
+      order: 
+        ['category_name'],   
+      attributes: ['id','category_name']   
+    });
+    const categories = categoryData.map((category) => {return category.get({ plain: true})})    
+    console.log(categories);
+    res.render('index', { 
+      categories,
+      loggedIn: req.session.loggedIn,
+     });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }   
  });    
 
 // // show login page to new user, logged in redirect to home page
---
+++ utils/helper.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Author: Mayur
+    // Function created decode HTML Entities such as &#039;
+    // using https://www.npmjs.com/package/he package
+    
+  };
+  
