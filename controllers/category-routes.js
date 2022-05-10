3d0794c6751e135b35e2ed96a5e9aee598f78d6e
Cody Bae Claussen <c.claussen01@gmail.com>
2022-05-10 12:34:58 -0500

    login/signup page

--- controllers/category-routes.js
+++ controllers/category-routes.js
@@ -19,46 +19,6 @@ const withAuth = require('../utils/auth'); //
       res.status(500).json(err);
      }
  });    
-
- 
- router.get('/', async (req, res) => {
-   try {
-     const categoryData = await Category.findAll({
-       order:
-         ['category_name'],
-       attributes: ['id', 'category_name']
-     });
-     const categories = categoryData.map((category) => {return category.get({ plain: true})})
-     console.log(categories);
-     res.render('index', {
-       categories,
-       loggedIn: req.session.loggedIn,
-      });
-   } catch (err) {
-     console.log(err);
-     res.status(500).json(err);
-   }
- });
- 
-// // show login page to new user, logged in redirect to home page
-// router.get('/login', (req, res) => {
-//     if (req.session.loggedIn) {
-//       res.redirect('/');
-//       return;
-//     }
-  
-//     res.render('login');
-//   });
-  
-//   // show signup page to new user, logged in redirect to home page
-//   router.get('/signup', (req, res) => {
-//     if (req.session.loggedIn) {
-//       res.redirect('/');
-//       return;
-//     }
-  
-//     res.render('signup');
-//   });
   
    module.exports = router;
 // Author: Mayur
