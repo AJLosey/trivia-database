322e0cace079b17e2dcebca4dab4f26905273697
Melissa Hookey <melissah315@gmail.com>
2022-05-11 14:09:38 -0500

    logo and links added

---
+++ public/home.js
@@ -0,0 +1,25 @@
+// trying to add radio and submit button functionality but it's still a work in progress
+
+const btn = document.querySelector('#submit-btn');        
+const radioButtons = document.querySelectorAll('input[name="flexRadio"]');
+
+btn.addEventListener("click", () => {
+    let selectedAnswer;
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selectedAnswer = radioButton.value;
+            break;
+        }
+    }
+});
+
+btn.addEventListener("submit", () => {
+    if (selectedAnswer === "Uruguay") {
+        output.innerText = `Correct!`;
+        console.log ('correct');
+    } else {
+        output.innerText = `Incorrect`;
+        console.log ('incorrect');
+    }
+})
+
