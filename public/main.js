85b3b2f03beb9911da9e086ed093a8912de6a25c
AJLosey <aaronlosey11@gmail.com>
2022-05-11 19:32:16 -0500

    stopped script from trying to attach event listener when logout button doesn't exist

--- public/main.js
+++ public/main.js
@@ -1,18 +1,20 @@
 
+if (document.querySelector('#logout-link')) {
+    const logout = async function () {
+        console.log('check')
+        const response = await fetch('/dashboard/logout', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        })
 
-const logout = async function () {
-    console.log('check')
-    const response = await fetch('/dashboard/logout', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    })
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log out.');
+        }
 
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert('Failed to log out.');
     }
 
-}
+    document.querySelector('#logout-link').addEventListener('click', logout);
 
-document.querySelector('#logout-link').addEventListener('click', logout);
+}
